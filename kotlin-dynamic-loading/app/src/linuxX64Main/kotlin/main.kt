ce30cc9e731f0ce7f5196299200b891604b85c51
Durun <negiigainuki@gmail.com>
2021-01-24 19:53:34 +0900

    add loader

--- kotlin-dynamic-loading/app/src/linuxX64Main/kotlin/main.kt
+++ kotlin-dynamic-loading/app/src/linuxX64Main/kotlin/main.kt
@@ -1,3 +1,33 @@
-fun main() {
+import kotlinx.cinterop.*
+import platform.posix.*
+
+fun main(args: Array<String>) {
 	println("Hello, world!")
+	check(args.size == 2) {
+		println("Usage: ./loader plugin func")
+	}
+	val (file, funcName) = args
+
+	val handle = runCatching {
+		dlopen(file, RTLD_LAZY)
+	}.onFailure {
+		println("(loader) Failed: open $file: $it")
+	}.getOrThrow()
+	println("(loader) Success: open $file")
+
+	val func = run {
+		dlerror()
+		val symbol = dlsym(handle, funcName)
+		val error = dlerror()
+		symbol.takeIf { error == null }?.reinterpret<CFunction<()->Int>>()
+			?: throw IllegalStateException("$error").also {
+				println("(loader) Failed: resolve $funcName: $error")
+			}
+	}
+
+	val v = func()
+	println("(loader) Success: call $funcName(): return value = $v")
+
+	dlclose(handle)
+	println("(loader) Success: Closed handle $file")
 }
